b845da305ba8a48c64a84b9942f28a049df1f149
Febrico Jonata <riko9647@gmail.com>
2023-10-29 00:06:54 +0700

    feat: talentDetail 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Talent from "./Components/Talent/Talent";
 import Cart from "./Components/Cart/Cart";
 import PaymentStatus from "./Components/Payment/PaymentStatus/PaymentStatus";
 import Payment from "./Components/Payment/Payment";
+import TalentDetail from "./Components/Talent/TalentDetail/TalentDetail";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="talent" element={<Talent />} />
+        <Route path="talent-detail" element={<TalentDetail />} />
         <Route path="cart" element={<Cart />} />
         <Route path="payment" element={<Payment />} />
         <Route path="payment-status" element={<PaymentStatus />} />
--- src/Components/Talent/TalentDetail/TalentDetail.jsx
+++ src/Components/Talent/TalentDetail/TalentDetail.jsx
@@ -1,5 +1,34 @@
+import "./TalentDetail.scss";
+
 const TalentDetail = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="talent-image">
+            <img
+              src="https://static.promediateknologi.id/crop/0x324:2160x1933/750x500/webp/photo/2022/10/12/722414333.jpg"
+              alt="talent"
+            />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="talent-content bg-warning">
+            <h3>Jo Yuri</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              consectetur recusandae autem quibusdam, pariatur nulla nobis esse
+              sunt omnis laborum non dolorem voluptates cumque a voluptatem
+              quisquam animi, illum exercitationem! Aliquam, harum modi vero
+              nemo placeat beatae doloribus cupiditate alias, ad officiis
+              dignissimos expedita minima at. Tempora illo eos laborum!
+            </p>
+            <button className="btn">Contact Me</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TalentDetail;
