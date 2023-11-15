962b030b5146d82dfa79f7333cf04b3a4ec6c135
Febrico Jonata <riko9647@gmail.com>
2023-11-15 16:49:24 +0700

    test deploy (2)

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Cart from "./Components/Cart/Cart";
 import PaymentStatus from "./Components/Payment/PaymentStatus/PaymentStatus";
 import Payment from "./Components/Payment/Payment";
 import TalentDetail from "./Components/Talent/TalentDetail/TalentDetail";
-// import Profile from "./Components/profile-module/profile"/
+import Profile from "./Components/Profile/profile";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         <Route path="cart" element={<Cart />} />
         <Route path="payment" element={<Payment />} />
         <Route path="payment-status" element={<PaymentStatus />} />
-        {/* <Route path="profile" element={<Profile />} /> */}
+        <Route path="profile" element={<Profile />} />
       </Routes>
     </Router>
   );
