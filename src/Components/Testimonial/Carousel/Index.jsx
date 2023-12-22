1c8bc0b44bced1a507b8ac22e589872b63828ea1
FebricoJ <riko967gmail.com>
2023-12-22 17:29:32 +0700

    add image to testimonial

--- src/Components/Testimonial/Carousel/Index.jsx
+++ src/Components/Testimonial/Carousel/Index.jsx
@@ -3,57 +3,6 @@ import Logo from "../../../Assets/Challenge-amico.png";
 
 export default function TestimonialCarousel() {
   return (
-    // <div
-    //   id="carouselExampleInterval"
-    //   class="carousel slide"
-    //   data-bs-ride="carousel"
-    // >
-    //   <div class="carousel-inner">
-    //     <div class="carousel-item active" data-bs-interval="10000">
-    //       <TestimonialContent
-    //         photo={Logo}
-    //         testimonial="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dignissimos maxime harum?"
-    //         name={"Febrico Jonata"}
-    //         position={"Software Engineer"}
-    //       />
-    //     </div>
-    //     <div class="carousel-item" data-bs-interval="2000">
-    //       <TestimonialContent
-    //         photo={Logo}
-    //         testimonial="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dignissimos maxime harum?"
-    //         name={"Febrico Jonata"}
-    //         position={"Software Engineer"}
-    //       />
-    //     </div>
-    //     <div class="carousel-item">
-    //       <TestimonialContent
-    //         photo={Logo}
-    //         testimonial="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dignissimos maxime harum?"
-    //         name={"Febrico Jonata"}
-    //         position={"Software Engineer"}
-    //       />
-    //     </div>
-    //   </div>
-    //   <button
-    //     class="carousel-control-prev"
-    //     type="button"
-    //     data-bs-target="#carouselExampleInterval"
-    //     data-bs-slide="prev"
-    //   >
-    //     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    //     <span class="visually-hidden">Previous</span>
-    //   </button>
-    //   <button
-    //     class="carousel-control-next"
-    //     type="button"
-    //     data-bs-target="#carouselExampleInterval"
-    //     data-bs-slide="next"
-    //   >
-    //     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    //     <span class="visually-hidden">Next</span>
-    //   </button>
-    // </div>
-
     <div
       id="carouselExampleIndicators"
       class="carousel slide carousel-dark"
@@ -84,30 +33,34 @@ export default function TestimonialCarousel() {
       <div class="carousel-inner">
         <div class="carousel-item active">
           <TestimonialContent
-            photo={Logo}
-            testimonial={"\"Incredible service! The team went above and beyond to meet our project requirements. Their expertise and dedication are truly commendable. Looking forward to future collaborations\""}
+            photo="https://t-2.tstatic.net/tribunnewswiki/foto/bank/images/William-Gozali-2.jpg"
+            testimonial={
+              '"Incredible service! The team went above and beyond to meet our project requirements. Their expertise and dedication are truly commendable. Looking forward to future collaborations"'
+            }
             name={"William Gozali"}
             position={"Restaurant Owner"}
           />
         </div>
         <div class="carousel-item">
           <TestimonialContent
-            photo={Logo}
-            testimonial={"\"I've had the pleasure of working with this team on multiple projects. Their commitment to quality and timely delivery is unmatched. I highly recommend their services.\""}
+            photo="https://th.bing.com/th/id/OIP.vdiepeaue9fyX_6UH86ztAHaEK?rs=1&pid=ImgDetMain"
+            testimonial={
+              '"I\'ve had the pleasure of working with this team on multiple projects. Their commitment to quality and timely delivery is unmatched. I highly recommend their services."'
+            }
             name={"Julian Alvarez"}
             position={"Football Player"}
           />
         </div>
         <div class="carousel-item">
           <TestimonialContent
-            photo={Logo}
-            testimonial={"\"Amazing service :) \""}
+            photo="https://asset.kompas.com/crops/IBXghVrqvVDo9sZvYTPhEbiErFM=/0x343:1000x1010/750x500/data/photo/2019/02/21/1025113456.jpg"
+            testimonial={'"Amazing service :) "'}
             name={"Jerome Polin"}
             position={"Influencer"}
           />
         </div>
       </div>
-      
+
       <button
         class="carousel-control-prev"
         type="button"
